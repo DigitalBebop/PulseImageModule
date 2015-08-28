92996be840c6ca3e692bf48dd630fc6913eca928
Joseph Batchik <josephbatchik@gmail.com>
2015-08-28 09:53:00 -0400

    moved ssl factory to own file

--- src/main/scala/net/digitalbebop/pulseimagemodule/Main.scala
+++ src/main/scala/net/digitalbebop/pulseimagemodule/Main.scala
@@ -27,25 +27,7 @@ import scala.collection.mutable
 import scala.concurrent.{Future, ExecutionContext}
 import scala.util.parsing.json.JSONObject
 
-class NaiveTrustManager extends X509TrustManager {
-  /**
-   * Doesn't throw an exception, so this is how it approves a certificate.
-   * @see javax.net.ssl.X509TrustManager#checkClientTrusted(java.security.cert.X509Certificate[], String)
-   **/
-  def checkClientTrusted(cert: Array[X509Certificate], authType: String): Unit = { }
 
-
-  /**
-   * Doesn't throw an exception, so this is how it approves a certificate.
-   * @see javax.net.ssl.X509TrustManager#checkServerTrusted(java.security.cert.X509Certificate[], String)
-   **/
-  def checkServerTrusted (cert: Array[X509Certificate], authType: String): Unit = { }
-
-  /**
-   * @see javax.net.ssl.X509TrustManager#getAcceptedIssuers()
-   **/
-   def getAcceptedIssuers: Array[X509Certificate] = Array()
-}
 
 object Main {
 
---
+++ src/main/scala/net/digitalbebop/pulseimagemodule/NaiveTrustManager.scala
@@ -0,0 +1,24 @@
+package net.digitalbebop.pulseimagemodule
+
+/**
+ * Created by jd on 8/28/15.
+ */
+class NaiveTrustManager extends X509TrustManager {
+  /**
+   * Doesn't throw an exception, so this is how it approves a certificate.
+   * @see javax.net.ssl.X509TrustManager#checkClientTrusted(java.security.cert.X509Certificate[], String)
+   **/
+  def checkClientTrusted(cert: Array[X509Certificate], authType: String): Unit = { }
+
+
+  /**
+   * Doesn't throw an exception, so this is how it approves a certificate.
+   * @see javax.net.ssl.X509TrustManager#checkServerTrusted(java.security.cert.X509Certificate[], String)
+   **/
+  def checkServerTrusted (cert: Array[X509Certificate], authType: String): Unit = { }
+
+  /**
+   * @see javax.net.ssl.X509TrustManager#getAcceptedIssuers()
+   **/
+   def getAcceptedIssuers: Array[X509Certificate] = Array()
+}
